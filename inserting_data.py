70739f8ba37b8683c448cd2456245256d9ead8eb
Alec Barran <alexanderbarran@gmail.com>
2024-04-04 22:12:29 -0700

    fix everyone's mistakes

--- inserting_data.py
+++ inserting_data.py
@@ -1,31 +1,29 @@
-import json
-import psycopg2
+import json, psycopg2
+from pathlib import Path
 
-def cleanStr4SQL(s):
+
+def cleanStr4SQL(s: str) -> str:
     return s.replace("'","`").replace("\n"," ")
 
-def int2BoolStr (value):
+def int2BoolStr(value: str) -> bool:
     if value == 0:
         return 'False'
     else:
         return 'True'
-dbname = ''
 
-password = ''
-def insert2BusinessTable():
-    #reading the JSON file
-    with open('./yelp_business.JSON', 'r') as f:
-        outfile = open('./yelp_business.SQL', 'w')  #uncomment this line if you are writing the INSERT statements to an output file.
-        line = f.readline()
-        count_line = 0
+def get_pg_config_str(config_path: Path) -> str:
+    with open(config_path, "r") as file:
+        data = json.load(file)
 
-        try:
-            conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-        except:
-            print('Unable to connect to the database!')
-        cur = conn.cursor()
+    return f"dbname='{data['dbname']}' user='{data['user']}' host='{data['host']}' password='{data['password']}'"
 
-        while line:
+# lots of code duplication for each insert... function but the values tuple makes it difficult
+def insert2BusinessTable(conn: psycopg2.extensions.connection, writeout=False):
+    cur = conn.cursor()
+
+    with open('./yelp_business.JSON', 'r') as f, open('./yelp_business.SQL', 'w') as outfile:
+        line_ct = 0
+        for line in f.readlines():
             data = json.loads(line)
             sql_str = "INSERT INTO Business (business_id, name, address, city, state, zipcode, stars, review_rating, review_count, num_checkins) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             values = (
@@ -38,88 +36,67 @@ def insert2BusinessTable():
                 data["stars"],
                 0.0,
                 data["review_count"],
-                0  
-                )
+                0
+            )
+
             try:
                 cur.execute(sql_str, values)
-            except:
-                print("Insert to Business failed!")
+            except Exception as e:
+                print(f"Insert to Business failed due to: {e}")
+                conn.rollback() # ensure to rollback on failure to keep the transaction state clean
+
             conn.commit()
-            
-            outfile.write(sql_str)
 
-            line = f.readline()
-            count_line +=1
+            if writeout:
+                outfile.write(sql_str)
 
-        cur.close()
-        conn.close()
+            line_ct +=1
 
-    print(count_line)
-    outfile.close()  
-    f.close()
+    print(f"Processed {line_ct} lines. Business loading complete.")
+    cur.close()
 
-def insert2ReviewTable():
-    
-    with open('./yelp_review.JSON', 'r') as f:
-        outfile = open('./yelp_review.SQL', 'w')  
-        line = f.readline()
-        count_line = 0
+def insert2ReviewTable(conn: psycopg2.extensions.connection, writeout=False):
+    cur = conn.cursor()
 
-        try:
-            conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-        except:
-            print('Unable to connect to the database!')
-        cur = conn.cursor()
-
-        while line:
+    with open('./yelp_review.JSON', 'r') as f, open('./yelp_review.SQL', 'w') as outfile:
+        line_ct = 0
+        for line in f.readlines():
             data = json.loads(line)
-            
+            sql_str = "INSERT INTO Review (review_id, user_id, business_id, stars, date, text, useful, funny, cool) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            values = (
+                cleanStr4SQL(data['review_id']),
+                cleanStr4SQL(data['user_id']),
+                cleanStr4SQL(data['business_id']),
+                data['stars'],
+                data['date'],  # Ensure this is in a format that PostgreSQL can accept as a date
+                data['text'],
+                data['useful'],
+                data['funny'],
+                data['cool']
+            )
+
             try:
-                sql_str = "INSERT INTO Review (review_id, user_id, business_id, stars, date, text, useful, funny, cool) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                values = (
-                    cleanStr4SQL(data['review_id']),
-                    cleanStr4SQL(data['user_id']),
-                    cleanStr4SQL(data['business_id']),
-                    data['stars'],
-                    data['date'],  # Ensure this is in a format that PostgreSQL can accept as a date
-                    data['text'],
-                    data['useful'],
-                    data['funny'],
-                    data['cool']
-                    )
                 cur.execute(sql_str, values)
             except Exception as e:
                 print(f"Insert to Review failed due to: {e}")
-                conn.rollback()  # Ensure to rollback on failure to keep the transaction state clean
+                conn.rollback()
 
             conn.commit()
-            
-            outfile.write(sql_str)
 
-            line = f.readline()
-            count_line +=1
+            if writeout:
+                outfile.write(sql_str)
 
-        cur.close()
-        conn.close()
+            line_ct +=1
 
-    print(count_line)
-    outfile.close()  
-    f.close()
+    print(f"Processed {line_ct} lines. Review loading complete.")
+    cur.close()
 
-def insert2UsersTable():
+def insert2UsersTable(conn: psycopg2.extensions.connection, writeout=False):
+    cur = conn.cursor()
 
-    with open('./yelp_user.JSON', 'r') as f:
-        outfile = open('./yelp_user.SQL', 'w')  
-        line = f.readline()
-        count_line = 0
-
-        try:
-            conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-        except:
-            print('Unable to connect to the database!')
-        cur = conn.cursor()
-
-        while line:
+    with open('./yelp_user.JSON', 'r') as f, open('./yelp_user.SQL', 'w') as outfile:
+        line_ct = 0
+        for line in f.readlines():
             data = json.loads(line)
             sql_str = "INSERT INTO Users (user_id, avg_stars, name, review_count, yelping_since) VALUES (%s, %s, %s, %s, %s)"
             values = (
@@ -127,78 +104,67 @@ def insert2UsersTable():
                 data['average_stars'],
                 cleanStr4SQL(data['name']),
                 data['review_count'],
-                data['yelping_since']  
+                data['yelping_since']
             )
 
             try:
                 cur.execute(sql_str, values)
-            except:
-                print("Insert to Users failed!")
+            except Exception as e:
+                print(f"Insert to Users failed due to: {e}")
+                conn.rollback()
+
             conn.commit()
-            
-            outfile.write(sql_str)
 
-            line = f.readline()
-            count_line +=1
+            if writeout:
+                outfile.write(sql_str)
 
-        cur.close()
-        conn.close()
+            line_ct +=1
 
-    print(count_line)
-    outfile.close()  
-    f.close()
+    print(f"Processed {line_ct} lines. Users loading complete.")
+    cur.close()
 
-def insert2CheckinTable():
-
-    with open('./yelp_checkin.JSON', 'r') as f:
-        
-        with open('./yelp_checkin.SQL', 'w') as outfile:
-            count_line = 0
-
-            try:
-                conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-                
-            except:
-                print('Unable to connect to the database!')
-            cur = conn.cursor()
-
-            for line in f:
-                data = json.loads(line)
-                business_id = cleanStr4SQL(data['business_id'])
-                for day, times in data['time'].items():
-                    for time, count in times.items():
-                        sql_str = """
+def insert2CheckinTable(conn: psycopg2.extensions.connection, writeout=False):
+    cur = conn.cursor()
+    with open('./yelp_checkin.JSON', 'r') as f, open('./yelp_checkin.SQL', 'w') as outfile:
+        line_ct = 0
+        for line in f.readlines():
+            data = json.loads(line)
+            business_id = cleanStr4SQL(data['business_id'])
+            for day, times in data['time'].items():
+                for time, count in times.items():
+                    sql_str = """
                         INSERT INTO Checkins (business_id, day, time, count)
                         VALUES (%s, %s, %s, %s)
                         ON CONFLICT (business_id, day, time)
                         DO UPDATE SET count = EXCLUDED.count;
-                        """
+                    """
+                    values = (
+                        business_id,
+                        day,
+                        time,
+                        count
+                    )
 
-                        values = (business_id, day, time, count)
+                    try:
+                        cur.execute(sql_str, values)
+                    except Exception as e:
+                        print(f"Insert to Checkin failed due to: {e}")
+                        conn.rollback()
 
-                        
-                        try:
-                            cur.execute(sql_str, values)
-                            conn.commit()  # Commit after each successful insert
-                        except:
-                            print("Insert to Checkin failed!")
-                        count_line += 1
+                    conn.commit()
 
-            cur.close()
-            conn.close()
+                    if writeout:
+                        outfile.write(sql_str)
 
-    print(count_line)
+                    line_ct +=1
 
-def insert2AttributesTable():
+    print(f"Processed {line_ct} lines. Checkins loading complete.")
+    cur.close()
 
-    try:
-        conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-    except:
-        print('Unable to connect to the database!')
+def insert2AttributesTable(conn: psycopg2.extensions.connection):
     cur = conn.cursor()
-
     with open('./yelp_business.JSON', 'r') as f:
-        for line in f:
+        for line in f.readlines():
             data = json.loads(line)
             business_id = data["business_id"]
             attributes = data.get("attributes", {})
@@ -214,8 +180,7 @@ def insert2AttributesTable():
 
             # Insert attributes into the database, only if attribute_value is True
             for attr_name, value in flat_attributes.items():
-                if value is True: 
-                    
+                if value:
                     sql_str = """
                     INSERT INTO Attributes (business_id, attr_name, value)
                     VALUES (%s, %s, %s)
@@ -229,26 +194,18 @@ def insert2AttributesTable():
                         print(f"ERROR - Insert failed for business_id {business_id}, attribute {attr_name}: {e}")
                         conn.rollback()  # Rollback in case of error
 
-    cur.close()
-    conn.close()
-
     print("Attributes loading complete.")
+    cur.close()
 
-def insert2CategoriesTable():
+def insert2CategoriesTable(conn: psycopg2.extensions.connection):
     """Parse categories from businesses and insert into the Categories table."""
-    try:
-        conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-    except:
-        print('Unable to connect to the database!')
     cur = conn.cursor()
-
     with open('./yelp_business.JSON', 'r') as f:
-        line_count = 0
-
-        for line in f:
+        line_ct = 0
+        for line in f.readlines():
             data = json.loads(line)
             business_id = cleanStr4SQL(data['business_id'])
-            
+
             if 'categories' in data and data['categories']:
                 categories = data['categories']  # Assume categories are already a list
 
@@ -264,21 +221,14 @@ def insert2CategoriesTable():
                         conn.commit()
                     except Exception as e:
                         print(f"ERROR - Insert failed for business_id {business_id}, category {cat_name}: {e}")
-                        conn.rollback() 
-            line_count += 1
+                        conn.rollback()
+            line_ct += 1
 
+    print(f"Processed {line_ct} lines. Categories loading complete.")
     cur.close()
-    conn.close()
-    print(f"Processed {line_count} lines. Categories loading complete.")
 
-def updateBusinessCheckins():
-    try:
-        conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-        cur = conn.cursor()
-    except:
-        print('Unable to connect to the database!')
-        return
-
+def updateBusinessCheckins(conn: psycopg2.extensions.connection):
+    cur = conn.cursor()
     update_sql = """
         UPDATE Business
         SET num_checkins = COALESCE((
@@ -295,58 +245,59 @@ def updateBusinessCheckins():
         conn.rollback()
 
     cur.close()
-    conn.close()
     print("num_checkins updated successfully.")
 
 
 def updateBusinessAggregateData():
+    cur = conn.cursor()
     try:
-        conn = psycopg2.connect(f"dbname={dbname} user='postgres' host='localhost' password={password}")
-        cur = conn.cursor()
-        
         # Step 1: Create a temporary table
         cur.execute("""
             CREATE TEMP TABLE IF NOT EXISTS temp_aggregate AS
-            SELECT 
+            SELECT
                 business_id,
                 COALESCE(COUNT(*), 0) AS review_count,
                 COALESCE(AVG(stars), 0) AS review_rating
             FROM Review
             GROUP BY business_id
         """)
-        
+
         # Step 2: Update the Business table using the temporary table
         cur.execute("""
             UPDATE Business
-            SET 
+            SET
                 review_count = temp_aggregate.review_count,
                 review_rating = temp_aggregate.review_rating
             FROM temp_aggregate
             WHERE Business.business_id = temp_aggregate.business_id
         """)
-        
+
         # Optionally: Drop the temporary table if you wish
         cur.execute("DROP TABLE temp_aggregate")
-        
+
         conn.commit()
-        print("Review Count and Review Rating Updated Successfully.")
-        
+
     except Exception as e:
         print(f"ERROR: {e}")
         conn.rollback()
-    finally:
-        cur.close()
-        conn.close()
 
-updateBusinessAggregateData()
+    cur.close()
+    print("Review Count and Review Rating Updated Successfully.")
 
+if __name__ == "__main__":
+    config_file = Path("pg_config.json")
+    config = get_pg_config_str(config_file)
+    try:
+        conn = psycopg2.connect(config)
+    except psycopg2.errors.OperationalError:
+        print("Unable to connect to the database!")
+        exit()
 
-
-insert2BusinessTable()
-insert2UsersTable()
-insert2CheckinTable()
-insert2ReviewTable()
-insert2AttributesTable()
-insert2CategoriesTable()
-updateBusinessCheckins()
-updateBusinessAggregateData()
+    insert2BusinessTable(conn)
+    insert2UsersTable(conn)
+    insert2CheckinTable(conn)
+    insert2ReviewTable(conn)
+    insert2AttributesTable(conn)
+    insert2CategoriesTable(conn)
+    updateBusinessCheckins(conn)
+    updateBusinessAggregateData(conn)
